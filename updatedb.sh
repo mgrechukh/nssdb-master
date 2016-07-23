6135cb305954ce678498369d452437308a175c7e
Mykola Grechukh <mgrechukh@satelliz.com>
2016-07-23 03:09:09 +0300

    getent is preferred over /etc/passwd reading

--- updatedb.sh
+++ updatedb.sh
@@ -7,11 +7,17 @@
 	cp /var/lib/misc/Makefile /nssdb/
 }
 
+## check also possibility to "merge" repositories
+# getent -s db shadow
+# because passwd(1) will not work if user exists only in db
+# doing administrative tasks in ephemeral container with persistent volume obviously require to populate back /etc/passwd always before recreating DB from it
+# or better resort to getent passwd | awk ....
+
 for i in passwd group; do
-	awk -F : '$1 !~ /^(guest|nobody)/ && $3 >= 1000 {print $0}' /etc/$i > /nssdb/etc/$i
+	getent $i | sort | uniq | awk -F : '$1 !~ /^(guest|nobody)/ && $3 >= 1000 {print $0}' > /nssdb/etc/$i
 done
 USERS=$(cat /nssdb/etc/passwd | cut -d: -f 1 | xargs | tr ' ' '|')
-grep -E "($USERS)" /etc/shadow > /nssdb/etc/shadow
+getent shadow | sort | uniq | grep -E "($USERS)" > /nssdb/etc/shadow
 	
 make -f /nssdb/Makefile
 
